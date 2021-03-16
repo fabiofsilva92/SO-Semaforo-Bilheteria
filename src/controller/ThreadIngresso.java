bbe3199a92ddf67f94b1df91ce2d2a2f4883bc97
fabiofsilva92 <fabiofsilva92@gmail.com>
2021-03-16 20:51:34 -0300

    Done

--- src/controller/ThreadIngresso.java
+++ src/controller/ThreadIngresso.java
@@ -1,51 +1,88 @@
 package controller;
 
-public class ThreadIngresso extends Thread{
+import java.util.concurrent.Semaphore;
+
+public class ThreadIngresso extends Thread {
+
 	private int idPessoa;
 	private static int ingressoDisponivel = 100;
-	
-	
-	public ThreadIngresso(int idPessoa) {
+	private Semaphore semaforo;
+
+	public ThreadIngresso(int idPessoa, Semaphore semaforo) {
 		this.idPessoa = idPessoa;
+		this.semaforo = semaforo;
 	}
-	
+
+	@Override
+	public void run() {
+		boolean verificador = loginSistema();
+
+		if (verificador) {
+			verificador = processoCompra();
+			if (verificador) {
+				try {
+					semaforo.acquire();
+					validacaoCompra();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaforo.release();
+				}
+
+			}
+		}
+
+	}
+
 	public boolean loginSistema() {
-		boolean verificador = false;
-		int tempo = (int)((Math.random()*1951)+50);
+		boolean verificador = true;
+		int tempo = (int) ((Math.random() * 1951) + 50);
+		
 		try {
 			sleep(tempo);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		if(tempo > 1000) {
-			System.out.println("#"+idPessoa+" -> Timeout");
-			verificador = true;
+		if (tempo > 1000) {
+			System.out.println("#" + idPessoa + " -> Timeout");
+			verificador = false;
 			return verificador;
 		}
-		System.out.println("#"+idPessoa+" -> Indo para o processo de compra");
+		System.out.println("#" + idPessoa + " -> Indo para o processo de compra");
 		return verificador;
 	}
-	
+
 	public boolean processoCompra() {
-		boolean verificador = false;
-		int tempo = (int) ((Math.random()*1001)+2000);
+		boolean verificador = true;
+		int tempo = (int) ((Math.random() * 1001) + 2000);
+		
 		try {
 			sleep(tempo);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if(tempo > 2500) {
-			System.out.println("#"+idPessoa+" --> Tempo de sessão esgotado");
-			verificador = true;
+		
+		if (tempo > 2500) {
+			System.out.println("#" + idPessoa + " --> Tempo de sessão esgotado");
+			verificador = false;
 			return verificador;
 		}
-		System.out.println("#"+idPessoa+" --> Indo para a validação de compra");
+		System.out.println("#" + idPessoa + " --> Indo para a validação de compra");
 		return verificador;
 	}
-	
+
 	public void validacaoCompra() {
+		int qtdIngressos = (int)((Math.random()*1.1) + 3);
 		
+		if(ingressoDisponivel > qtdIngressos) {
+			System.out.println("#"+idPessoa+" conseguiu comprar "+qtdIngressos+" ingressos");
+			ingressoDisponivel = ingressoDisponivel - qtdIngressos;
+			System.out.println("Ainda restam "+ingressoDisponivel+" ingressos.");
+		}
+		else {
+			System.out.println("Não há ingressos disponiveis.");
+		}
 	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,21 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadIngresso;
+
 public class Principal {
+
+	public static void main(String[] args) {
+		
+		int permissoes = 100;
+		Semaphore semaforo = new Semaphore(permissoes);
+		
+		for(int i = 0; i<300; i++) {
+			Thread TIngresso = new ThreadIngresso(i, semaforo);
+			TIngresso.start();
+		}
+
+	}
 
 }
